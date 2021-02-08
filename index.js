6d4bade11ce838d57f2769fe18ea276e9557558f
talryan <nataliamryan@gmail.com>
2021-02-08 09:10:37 -0800

    commit tree b34ae93a063f7b257b890e5afa9d2c8b32a54645

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+
+function findMatching(drivers, string){
+    let match = drivers.filter( driver => {
+        return driver === string || driver === string.toLowerCase()
+    })
+   return match 
+}
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+  }
+
+function matchName(drivers, string){
+    return drivers.filter(function(object) { 
+        if (object["name"] === string){
+        return object 
+    }
+    })
+
+}
